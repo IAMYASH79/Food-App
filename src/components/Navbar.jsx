ff45bf2048c5183ce991c4b5304d2128e716c7c4
yash <yash9570raj@gmail.com>
2024-09-04 22:42:25 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200">
+        <div className="container">
+          <div className="flex justify-center items-center">
+            <div>
+              <a href="#">
+                <img src="" alt="" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
