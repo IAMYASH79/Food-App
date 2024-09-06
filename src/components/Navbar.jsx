3f421f01becca3630ac458d73421fd9a45d693f2
yash <yash9570raj@gmail.com>
2024-09-06 21:09:42 +0530

    add some desiging

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,53 @@
 import React from "react";
+import Logo from "../assets/food-logo.png";
 
 const Navbar = () => {
   return (
     <>
       <div className="shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200">
         <div className="container">
-          <div className="flex justify-center items-center">
+          <div className="flex justify-between items-center">
             <div>
-              <a href="#">
-                <img src="" alt="" />
+              <a
+                href="#"
+                className="flex items-center gap-2 text-2xl sm:text-3xl font-bold"
+              >
+                <img src={Logo} alt="Foodie Zone" className="w-10" />
+                Foodie
               </a>
             </div>
+          </div>
+
+          <div>
+            <ul className=" hidden sm:flex gap-4">
+              <li>
+                <a
+                  href="#"
+                  className="inline-block py-4 px-4 hover:text-primary"
+                >
+                  Home
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="inline-block py-4 px-4 hover:text-primary"
+                >
+                  About
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="inline-block py-4 px-4 hover:text-primary"
+                >
+                  Contact
+                </a>
+              </li>
+            </ul>
+            <button className="bg-gradient-to-r from-primary to-secondary text-white px-4 py-1 rounded-md ">
+              Order
+            </button>
           </div>
         </div>
       </div>
