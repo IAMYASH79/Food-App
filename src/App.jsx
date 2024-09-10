973d76e8e54b6061132f10b499bdbd6c5ef0fa88
yash <yash9570raj@gmail.com>
2024-09-10 20:08:35 +0530

    fix footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
-
 import Banner from "./components/Banner/Banner";
 import Footer from "./components/Footer/Footer";
 
